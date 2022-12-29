eb0eba641fe626ea5b213883c28133d689b8c3dd
JasonYebra <jasonyebra@gmail.com>
2022-12-29 21:36:39 +0000

    Saved progress on Instantiation 2022-12-29 21:36:39 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
+        Object this = new Object()
         return null;
     }
 }
